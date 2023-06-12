eca8dfcf31e3e9a14692786972085876d9901df0
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-12 14:54:50 +0200

    Finish task2

--- js/functions.js
+++ js/functions.js
@@ -1,19 +1,20 @@
 // Функция проверки длинны строки
 
-function checkLengthString (string, numb) {
+function checkLengthString (string, num) {
 
-  if (string.length <= numb) {
+  if (string.length <= num) {
     return true;
   }
   return false;
 }
+/*console.log(checkLengthString('home12.9', 8))*/
 
 //Фунция проверки на палидром
 
-function checkPalindrome(stringPalindrome) {
-  const newString1 = stringPalindrome.replaceAll(' ', '').toLowerCase(); // убираем пробели + нижний регистр
+function checkPalindrome (string) {
+  const newString1 = string.replaceAll(' ', '').toLowerCase(); // убираем пробели + нижний регистр
   let newString2 = '';
-  for (let i = newString1.length - 1; i >= 0; i--) { // перебераем с конца
+  for (let i = newString1.length - 1; i >= 0; i--) { // перебираем с конца
     newString2 += newString1[i]; // записываем в новую переменную
   }
   if(newString1 === newString2) { // сравниваем и возвращаем булево значение
@@ -21,5 +22,22 @@ function checkPalindrome(stringPalindrome) {
   }
   return false;
 }
+/*console.log(checkPalindrome ('WoW'))*/
 
+// Функция отделения числа
 
+function takeNumberFromString (string) { // Принимаем строку
+  let newNum = ''; // создаём переменную строку
+  for(let i = 0; i < string.length; i++) { //перебираем строку
+    const num = parseInt(string[i]); // приводим каждый символ к числу
+
+    if (Number.isNaN(num) === false) { // проверяем на NaN
+      newNum += num; // вписываем в переменную если это число
+    }
+  }
+  if (newNum.length < 1) { // Проверяем если длина строки меньше 1 (строка пустая)
+    return NaN; // возвращаем NaN
+  }
+  return Number(newNum); // возвращаем переменную ввиде числа
+}
+/*console.log(takeNumberFromString ('45ee,1.5 - -5'));*/
