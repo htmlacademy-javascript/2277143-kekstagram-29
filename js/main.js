b2fbf0f909fe8e1ce568cd77b05193211c86aa1c
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-16 14:36:03 +0200

    add comments ok

--- js/main.js
+++ js/main.js
@@ -55,12 +55,12 @@ const exampleNames = [
 ];
 
 const exampleMessage = [
-  '11111',
-  '2222222',
-  '333333.',
-  '444444',
-  '5555555',
-  '66666666'
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
 ];
 
 
@@ -92,33 +92,23 @@ function createInfoFoto() { // Функция по созданию каточк
     const getNumberComments = getRandomInteger(0, 30);// Получаем номер коментариев
 
     for(let i = 0; i < getNumberComments; i++) {
+
       const getNumberAvatar = getRandomInteger(1, 6); // Получаем номер аватара
-      let getQuantityrMessage = getRandomInteger(1, 2); // Получаем номер коментрариев
+      const getQuantityrMessage = getRandomInteger(1, 2); // Получаем номер коментрариев
 
-
-
-      function addCom (min, max) {
+      const addCom = function () {
         let strokeMassege = '';
-        for(let i = 0; i < getQuantityrMessage; i++) {
-          let getMessage = exampleMessage[getRandomInteger(0, exampleMessage.length - 1)]; // Получаем cлучайный коментарий из массива
-          strokeMassege = strokeMassege + getMessage + ' '
+        for(let j = 0; j < getQuantityrMessage; j++) {
+          const getMessage = exampleMessage[getRandomInteger(0, exampleMessage.length - 1)]; // Получаем cлучайный коментарий из массива
+          strokeMassege = `${strokeMassege + getMessage } `;
         }
-        return strokeMassege
-      }
-
-
-
-
-
-
-
-
-
+        return strokeMassege;
+      };
 
       arrComments.push({
         id: generateIdComment(),
         avatar: `img/avatar-${ getNumberAvatar }.svg`,
-        message: addCom(1, 2),
+        message: addCom()
       });
     }
     return arrComments;
@@ -134,4 +124,4 @@ function createInfoFoto() { // Функция по созданию каточк
 }
 
 console.log(createInfoFoto());
-/*console.log(Array.from({length: 5}, createInfoFoto));*/
+console.log(Array.from({length: 5}, createInfoFoto));
