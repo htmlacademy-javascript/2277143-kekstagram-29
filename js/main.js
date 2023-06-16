d2d0ec32e204b694a1f153b2192ea9b4174759cc
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-16 18:29:36 +0200

    4 home work - ok

--- js/main.js
+++ js/main.js
@@ -73,4 +73,4 @@ function createInfoFoto() { // ФУНКЦИЯ БЛОКА ФОТОГРАФИИ
     comments: makeComments()
   };
 }
-console.log(Array.from({length: 3}, createInfoFoto));
+console.log(Array.from({length: 25}, createInfoFoto));
