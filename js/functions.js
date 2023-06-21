02df9251f3c27ca6189f66cd5dd953b6615a569e
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-21 14:53:06 +0200

    Add 2 function for calculation time of meeting

--- js/functions.js
+++ js/functions.js
@@ -51,3 +51,57 @@ function takeNumberFromString (string) {
   return Number(newNum); // если строка не пустая возвращаем переменную ввиде числа
 }
 takeNumberFromString ('45ee,1.er5 - 0-5');
+
+
+/**
+ *
+ * @param {string} time
+ * @returns {number} минут
+ */
+const calcMinutesFromTime = function(time) {
+  const elementsOfTime = time.split(':').map(Number);
+  const [hours, minutes] = elementsOfTime;
+  const minutesPerHour = 60;
+  return hours * minutesPerHour + minutes;
+};
+
+/**
+ *
+ * @param {string} startDay
+ * @param {string} finishDay
+ * @param {string} startMeet
+ * @param {number} timeMeet
+ * @returns булево значение если встреча пройдет в срок
+ */
+const calcOfMeet1 = function(startDay, finishDay, startMeet, timeMeet) {
+  const minutesStartDay = calcMinutesFromTime(startDay);
+  const minutesFinishtDay = calcMinutesFromTime(finishDay);
+  const minutesStartMeet = calcMinutesFromTime(startMeet);
+  if(minutesStartDay <= minutesStartMeet && minutesStartMeet + timeMeet <= minutesFinishtDay) {
+    return true;
+  } else {
+    return false;
+  }
+};
+calcOfMeet1('08:00', '17:30', '14:00', 90);
+
+/**
+ *
+ * @param {string} startDay
+ * @param {string} finishDay
+ * @param {string} startMeet
+ * @param {string} timeMeet
+ * @returns булево значение если встреча пройдет в срок
+ */
+const calcOfMeet2 = function(startDay, finishDay, startMeet, timeMeet) {
+  const minutesPerHour = 60;
+  const minutesStartDay = Number(startDay.split(':')[0]) * minutesPerHour + Number(startDay.split(':')[1]);
+  const minutesFinishtDay = Number(finishDay.split(':')[0]) * minutesPerHour + Number(finishDay.split(':')[1]);
+  const minutesStartMeet = Number(startMeet.split(':')[0]) * minutesPerHour + Number(startMeet.split(':')[1]);
+  if(minutesStartDay <= minutesStartMeet && minutesStartMeet + timeMeet <= minutesFinishtDay) {
+    return true;
+  } else {
+    return false;
+  }
+};
+calcOfMeet2('08:00', '17:30', '14:00', 90);
