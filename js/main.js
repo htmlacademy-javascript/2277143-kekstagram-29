3726e66ce51bc246c12cdd07c8494bbcdfbddc82
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-22 14:10:12 +0200

    change appercase

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
 import {createInfoFoto} from './data.js';
-const QUANTITY_OBJECT_IN_ARRAY = 25;
-console.log(Array.from({length: QUANTITY_OBJECT_IN_ARRAY}, createInfoFoto));
+const quantityObjectsInArray = 25;
+console.log(Array.from({length: quantityObjectsInArray}, createInfoFoto));
