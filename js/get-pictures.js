06c9c8c03ff7c4c297bf73504c5f7a2629cfc50e
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-26 17:44:39 +0200

    add Template

---
+++ js/get-pictures.js
@@ -0,0 +1,7 @@
+import {createInfoFoto} from './data.js';
+
+const listPistures = document.querySelector('.pictures'); // куда вставить
+const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture'); // шаблон
+
+console.log(pictureTemplate);
+
