842198784a9729e8d5b5be87a7b1cad178b124e4
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-06-29 18:25:03 +0200

    Update get-pictures.js

--- js/get-pictures.js
+++ js/get-pictures.js
@@ -2,14 +2,14 @@ import {createInfoPhotos} from './data.js';
 
 const photoContainer = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
-const constructPictures = createInfoPhotos();
+const constructPicturesArray = createInfoPhotos();
 
 const photosFragment = document.createDocumentFragment();
 
 /**
  * Функция клонирует шаблон, заполняет его данными и вставляет в DocumentFragment
  */
-constructPictures.forEach((picture) => {
+constructPicturesArray.forEach((picture) => {
   const photoCloneTemplate = photoTemplate.cloneNode(true);
   const photoImage = photoCloneTemplate.querySelector('.picture__img');
   photoImage.src = picture.url;
