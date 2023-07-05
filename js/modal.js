ecf3d204035fc82ba6206f4dd4a53771b63daccd
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-05 14:12:08 +0200

    add coments for big foto

--- js/modal.js
+++ js/modal.js
@@ -9,18 +9,37 @@ const likesBigPhoto = document.querySelector('.likes-count');
 const imgBigPhoto = document.querySelector('.big-picture__img img');
 const commentsNumberBigPhoto = document.querySelector('.comments-count');
 const descriptionBigPhoto = document.querySelector('.social__caption');
-
 const pictureComments = document.querySelector('.social__comments');
+
+pictureComments.innerHTML = '';
 
 const closeModal = function() {
   bigPhoto.classList.add('hidden');
-  closePhoto.removeEventListener('click', closeModal)
+  closePhoto.removeEventListener('click', closeModal);
+  pictureComments.innerHTML = '';
 };
 
 const closeEsc = function(evt) {
   evt.preventDefault();
   closeModal();
 };
+
+function fillComment(photo) {
+  photo.comments.forEach((comment) => {
+    const element = document.createElement('li');
+    const img = document.createElement('img');
+    const text = document.createElement('p');
+    element.classList.add('social__comment');
+    img.classList.add('social__picture');
+    text.classList.add('social__text');
+    img.src = comment.avatar;
+    img.alt = comment.name;
+    text.textContent = comment.message;
+    pictureComments.appendChild(element);
+    element.appendChild(img);
+    element.appendChild(text);
+  });
+}
 
 const openModal = function(element, photo) {
   element.addEventListener('click', () => {
@@ -29,8 +48,10 @@ const openModal = function(element, photo) {
     likesBigPhoto.textContent = photo.likes;
     commentsNumberBigPhoto.textContent = photo.comments.length;
     descriptionBigPhoto.textContent = photo.description;
-    closePhoto.addEventListener('click', closeModal);
 
+    fillComment(photo);
+
+    closePhoto.addEventListener('click', closeModal);
     document.addEventListener('keydown', closeEsc);
   });
 };
