8949b9778bad8943fa22722375b2cbc63c0fb6f5
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-08 13:04:49 +0200

    New modal option (better)

--- js/modal.js
+++ js/modal.js
@@ -1,10 +1,11 @@
 import {constructPicturesArray} from './get-pictures.js';
 
+const COMMENT_PER_PORTION = 5;
 const rowArray = constructPicturesArray;
 const body = document.querySelector('body');
 const minArrPhotos = document.querySelectorAll('.picture'); // массив картинок
 const bigPhoto = document.querySelector('.big-picture'); // modal
-const closePhoto = bigPhoto.querySelector('.big-picture__cancel'); // кнопка закрыть
+const closeButton = bigPhoto.querySelector('.big-picture__cancel'); // кнопка закрыть
 
 const likesBigPhoto = bigPhoto.querySelector('.likes-count');
 const imgBigPhoto = bigPhoto.querySelector('.big-picture__img img');
@@ -14,25 +15,30 @@ const descriptionBigPhoto = bigPhoto.querySelector('.social__caption');
 const commentsContainerBigPhoto = bigPhoto.querySelector('.social__comments');
 const commentsCountBigPhoto = bigPhoto.querySelector('.social__comment-count');
 const commentsLoaderBigPhoto = bigPhoto.querySelector('.comments-loader');
+let commentsShowArray = [];
 
-let commentsShown = 0;
+const onPictureEsc = function(evt) {
+  if(evt.key === 'Escape'){
+    closeModal();
+  }
+};
 
+/**
+Функция закрытия большой картинки
+*/
 const closeModal = function() {
   body.classList.remove('modal-open');
   bigPhoto.classList.add('hidden');
-  closePhoto.removeEventListener('click', closeModal);
-  document.removeEventListener('keydown', closeEsc);
-  commentsLoaderBigPhoto.removeEventListener('click', showMore);
-  bigPhoto.removeEventListener('click', onOverlayClick);
+  document.removeEventListener('keydown', onPictureEsc);
+  closeButton.removeEventListener('click', closeModal);
+  commentsLoaderBigPhoto.removeEventListener('click', showMoreComments);
 };
 
-const closeEsc = function() {
-  closeModal();
-  document.removeEventListener('keydown', closeEsc);
-  closePhoto.removeEventListener('click', closeModal);
-};
-
-const fillComment = function({comments}) {
+/**
+ * Функция генерации комментариев
+ * @param {Array} comments - массив комментариев из объекта
+ */
+const createPictureComments = function(comments) {
   comments.forEach((comment) => {
     const element = document.createElement('li');
     const img = document.createElement('img');
@@ -50,61 +56,79 @@ const fillComment = function({comments}) {
 };
 
 /**
- * функция скрытия кнопки если не осталось коментариев
- */
-const hideShowMoreButton = function() {
-  if(commentsContainerBigPhoto.children.length === commentsShown.length) {
-    commentsLoaderBigPhoto.classList.add('hidden');
-  } else {
-    commentsLoaderBigPhoto.classList.remove('hidden');
-  }
-};
-
-/**
- * функция скрытия коментариев после 5
- */
-const hideComments = function() {
-  for(let i = 5; i < commentsContainerBigPhoto.children.length; i++) {
-    commentsContainerBigPhoto.children[i].classList.add('hidden');
-  }
-  commentsShown = bigPhoto.querySelectorAll('.social__comment:not(.hidden)');
-  hideShowMoreButton();
-};
-
-/**
  * функция добавления ещё 5 коментариев
  */
-const showMore = function() {
-  for(let i = commentsShown.length; i < (commentsShown.length + 5) && i < commentsContainerBigPhoto.children.length; i++) {
-    commentsContainerBigPhoto.children[i].classList.remove('hidden');
+const showMoreComments = function() {
+  // Если длинна массива с комментариями равно 0 то ни чего не делаем
+  if (!commentsShowArray.length) {
+    return;
   }
-  commentsShown = bigPhoto.querySelectorAll('.social__comment:not(.hidden)');
-  commentsCountBigPhoto.textContent = `${commentsShown.length} из ${commentsContainerBigPhoto.children.length} комментариев`;
-  hideShowMoreButton();
+  // Обрезаем наш массив с комментариями. Сначала берем то количество комментарием
+  // что уже отрисовано, и прибавляем к ним 5, и получаем новые элементы котоыре
+  // нужно дорисовать в список
+  const additionalComments = commentsShowArray.slice(commentsContainerBigPhoto.children.
+    length, commentsContainerBigPhoto.children.length + COMMENT_PER_PORTION);
+
+  // Добовляем комментарии в уже существующий список
+  createPictureComments(additionalComments);
+
+  // Показываем кол-во отрисованных коментариев
+  commentsCountBigPhoto.textContent =
+   `${commentsContainerBigPhoto.children.length} из ${commentsShowArray.length} комментариев`;
+
+  // Если больше отрисовывать нечего, прячем кнопку
+  if (commentsShowArray.length <= commentsContainerBigPhoto.children.length) {
+    commentsLoaderBigPhoto.classList.add('hidden');
+  }
 };
 
-const onOverlayClick = function(evt) {
-  if (!evt.target.closest('.big-picture__preview') || evt.target.closest('.big-picture__cancel')) {
-    closeModal();
+/**
+Функция отрисовки начальных комментариев. Именно тех, которые мервые пять штука отрисовываются при открытие модального окна
+* @param {array} comments - массив комментариев из объекта, дестроктуризация тут
+*/
+function fillComments({comments}) {
+  const showFirstComments = comments.slice(0, COMMENT_PER_PORTION);
+
+  createPictureComments(showFirstComments);
+
+  commentsCountBigPhoto.textContent = `${showFirstComments.length} из ${comments.length} комментариев`;
+
+  // Если изначально у нас меньше или равно 5 комментариев, то прячем кнопку и счетчик комментариев
+  if (showFirstComments.length >= comments.length) {
+    commentsCountBigPhoto.classList.add('hidden');
+    commentsLoaderBigPhoto.classList.add('hidden');
   }
-};
+}
+
+/**
+Функция открытия большой картинки при клике на миниатюру
+* @param {string} picture - DOM элемент миниатюры, по который мы кликнули
+* @param {object} item - объект одной картинки, которую мы генирировали в data. Сюда передается именно объект той миниатюры по который мы кликнули. Передаем сюда этот объект в файле main.js
+*/
 
 const openModal = function(element, photo) {
   element.addEventListener('click', () => {
     commentsContainerBigPhoto.innerHTML = '';
+    commentsCountBigPhoto.classList.remove('hidden');
+    commentsLoaderBigPhoto.classList.remove('hidden');
+
+    // Тут мы созданному массиву присваиваем массив комментариев из объектаù
+    commentsShowArray = photo.comments;
     body.classList.add('modal-open');
     bigPhoto.classList.remove('hidden');
     imgBigPhoto.src = photo.url;
     likesBigPhoto.textContent = photo.likes;
     commentsNumberBigPhoto.textContent = photo.comments.length;
     descriptionBigPhoto.textContent = photo.description;
-    bigPhoto.addEventListener('click', onOverlayClick);
-    fillComment(photo);
-    hideComments();
-    closePhoto.addEventListener('click', closeModal);
-    document.addEventListener('keydown', closeEsc);
-    commentsCountBigPhoto.textContent = `${commentsShown.length} из ${commentsContainerBigPhoto.children.length} комментариев`;
-    commentsLoaderBigPhoto.addEventListener('click', showMore);
+
+    // При открытие вешаем на кнопку "Загрузить еще" обработчик, которые будет дорисовывать комментарии
+    commentsLoaderBigPhoto.addEventListener('click', showMoreComments);
+
+    // Вызываем функцию отрисовки начальных комментариев
+    fillComments(photo);
+    document.addEventListener('keydown', onPictureEsc);
+    closeButton.addEventListener('click', closeModal);
+
   });
 };
 
