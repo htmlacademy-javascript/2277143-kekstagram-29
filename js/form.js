129861dba9e2c17aeda8bd1bb7ca92f46fbb2ef2
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-10 13:35:55 +0200

    work open / close form (new foto)

--- js/form.js
+++ js/form.js
@@ -4,13 +4,30 @@ const upload = document.querySelector('.img-upload');
 const uploadInput = upload.querySelector('.img-upload__input');
 const uploadOverlay = upload.querySelector('.img-upload__overlay');
 const uploadForm = upload.querySelector('.img-upload__form');
+const closeButton = upload.querySelector('.img-upload__cancel');
 
+
+const onPictureEsc = function(evt) {
+  if(evt.key === 'Escape'){
+    closeModal();
+  }
+};
+
+function closeModal () {
+  body.classList.remove('modal-open');
+  uploadOverlay.classList.add('hidden');
+  document.removeEventListener('keydown', onPictureEsc);
+  closeButton.removeEventListener('click', closeModal);
+}
 
 const openForm = function() {
   body.classList.add('modal-open');
   uploadOverlay.classList.remove('hidden');
+  document.addEventListener('keydown', onPictureEsc);
+  closeButton.addEventListener('click', closeModal);
 };
 
 uploadInput.addEventListener('input', () => {
   openForm();
 });
+
