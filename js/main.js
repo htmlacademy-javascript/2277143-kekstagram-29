9b5b2d1f3c18ae2579efbc1aecf7ee0a7437e79a
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-13 11:10:44 +0200

    create module for scale image

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import './get-pictures.js'; // возможно не нужна?
 import {openPhoto} from './modal.js';
 import {onUploadFoto} from './form.js';
+import './scale-of-image.js';
 
 openPhoto();
 onUploadFoto();
