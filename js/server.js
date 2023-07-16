c1c9d904ec83ca222e63543e4d263cd556f36d71
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-16 13:27:34 +0200

    function of get data from server

--- js/server.js
+++ js/server.js
@@ -1,14 +1,23 @@
-fetch('https://29.javascript.pages.academy/kekstagram',
-  {
-    method: 'POST',
+
+const URL_BASE = 'https://29.javascript.pages.academy/kekstagram/data';
+
+const createLoader = (onSuccess, onError) => () => fetch(URL_BASE, {
+    method: 'GET',
     credentials: 'same-origin',
-    body: new FormData(),
-  }
-)
-  .then((response) => response.json())
-  .then((data) => {
-    console.log('Ответ', data);
-  });
+  })
+    .then((response) => {
+      if(response.ok) {
+        return response.json();
+      }
+      throw new Error(`${response.status} ${response.statusText}`);
+    })
+    .then((data) => {
+      console.log('Результат', data);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+export {createLoader};
 
 
 
