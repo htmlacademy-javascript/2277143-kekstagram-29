7e0c53702b48551506a07613eb00c2d83ff773fb
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-17 14:06:29 +0200

    add server.js code

--- js/server.js
+++ js/server.js
@@ -1,25 +1,37 @@
 
-const URL_BASE = 'https://29.javascript.pages.academy/kekstagram/data';
+const URL_BASE = 'https://29.javascript.pages.academy/kekstagram';
 
-const createLoader = (onSuccess, onError) => () => fetch(URL_BASE, {
-    method: 'GET',
-    credentials: 'same-origin',
-  })
+const Route = {
+  GET_DATA: '/data',
+  SEND_DATA: '/',
+};
+
+const Method = {
+  GET: 'GET',
+  POST: 'POST'
+};
+
+const ErroreText = {
+  GET_DATA: 'Не удвалось загрузить данные. Попробуйте обновить страницу',
+  SEND_DATA: 'Не удалось отправить форму, Попробуйте ещё раз',
+};
+
+const load = (route, errorText, method = Method.GET, body = null) =>
+  fetch(`${URL_BASE}${route}`, {method, body})
     .then((response) => {
-      if(response.ok) {
-        return response.json();
+      if (!response.ok) {
+        throw new Error();
       }
-      throw new Error(`${response.status} ${response.statusText}`);
+      return response.json();
     })
-    .then((data) => {
-      console.log('Результат', data);
-    })
-    .catch((err) => {
-      console.error(err);
-    })
-export {createLoader};
+    .catch(() => {
+      throw new Error(errorText);
+    });
 
+const getData = () => load(Route.GET_DATA, ErroreText.GET_DATA);
 
+const sendData = (body) => load(Route.SEND_DATA,ErroreText.SEND_DATA, Method.POST, body);
 
+export {getData, sendData};
+ 
 
-//console.log('+++');
