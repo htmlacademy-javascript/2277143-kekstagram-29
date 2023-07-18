c1e32548a0c75526304c9fbe52f2c8121cd33f03
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-18 15:24:49 +0200

    change main

--- js/main.js
+++ js/main.js
@@ -2,7 +2,30 @@ import './get-pictures.js'; // возможно не нужна?
 import {openPhoto} from './modal.js';
 import {onUploadFoto} from './form.js';
 import './get-effect.js';
-import './server.js';
+
+import { getData, sendData } from './server.js';
+import {showAlert} from './alert.js';
+
+/*
+function (data) {
+  sendData(data)
+    .then(() => {
+      closeForm ();
+      showSuccessMessage();
+    })
+    .catch(() => {
+      showErrorMessage();
+    });
+}
+*/
+
+try {
+  const data = await getData();
+  openPhoto(data);
+} catch (err) {
+  showAlert(err.message);
+}
+
 
 openPhoto();
 onUploadFoto();
