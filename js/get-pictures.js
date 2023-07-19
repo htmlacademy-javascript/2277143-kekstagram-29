7376ab4f07d1a692946e63aa940b6cc8600733ce
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-19 14:22:40 +0200

    delete space

--- js/get-pictures.js
+++ js/get-pictures.js
@@ -3,13 +3,11 @@ import {createInfoPhotos} from './data.js';
 const photoContainer = document.querySelector('.pictures');
 const photoTemplate = document.querySelector('#picture').content.querySelector('.picture');
 const constructPicturesArray = createInfoPhotos();
-
 const photosFragment = document.createDocumentFragment();
 
 /**
  * Функция клонирует шаблон, заполняет его данными и вставляет в DocumentFragment
  */
-
 const createThumbnails = (data) => {
   data.forEach((picture) => {
     const photoCloneTemplate = photoTemplate.cloneNode(true);
@@ -21,7 +19,6 @@ const createThumbnails = (data) => {
     photoCloneTemplate.querySelector('.picture__comments').textContent = picture.comments.length;
     photosFragment.appendChild(photoCloneTemplate);
   });
-
   photoContainer.appendChild(photosFragment);
 };
 
