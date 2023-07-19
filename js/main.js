05326aba9baaf8e01a089093ef0aeb58dfb08ad9
Aleksei Demianenko <aleksey.demianenko@gmail.com>
2023-07-19 14:10:02 +0200

    delete space from main.js

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 import {onUploadFoto, createSendForm, closeForm, unblockSubmitButton} from './form.js';
 import './get-effect.js';
 import {renderGallery} from './galery.js';
-import { getData, sendData } from './server.js';
+import {getData, sendData} from './server.js';
 import {showAlert} from './alert.js';
-import { showSuccessMessage, showErrorMessage } from './message.js';
-
+import {showSuccessMessage, showErrorMessage} from './message.js';
 
 createSendForm(async (data) => {
   try {
